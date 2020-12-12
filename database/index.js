9683eaa9812e748eac57f950c4e67b69f5c27ae1
Genaro V Salinas <gvsalinas3@gmail.com>
2020-12-12 12:17:09 -0800

    Used the pool object from the pg node driver.

--- database/index.js
+++ database/index.js
@@ -1,58 +1,80 @@
 const { database, user, password, address } = require('./config/config.js');
 const Query = require('./queries.js');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client(`postgres://${user}:${password}@${address}/${database}`);
-
-(async function createConnection() {
-  await client.connect();
-})();
+const pool = new Pool({
+  connectionString: `postgres://${user}:${password}@${address}/${database}`
+});
 
 const getDataWithPType = (productId) => {
-  return client.query('SELECT type_id FROM products WHERE id = $1;', [productId])
-    .then(result => {
-      var type = result.rows[0].type_id;
-      return client.query(Query.PTypeQuery, [type]);
+  return pool.connect()
+    .then(client => {
+      return client.query('SELECT type_id FROM products WHERE id = $1;', [productId])
+        .then(result => {
+          var type = result.rows[0].type_id;
+          return client.query(Query.PTypeQuery, [type]);
+        })
+        .then(results => {
+          client.release();
+          return results;
+        });
     })
     .catch(err => {
+      client.release();
       console.log(err);
     });
 };
 
-const getDataWithCategory = (productId, cb) => {
-  return client.query('SELECT category_id FROM products WHERE id = $1;', [productId])
-    .then(result => {
-      var category = result.rows[0].category_id;
-      return client.query(Query.CategoryQuery, [category]);
+const getDataWithCategory = (productId) => {
+  return pool.connect()
+    .then(client => {
+      return client.query('SELECT category_id FROM products WHERE id = $1;', [productId])
+        .then(result => {
+          var category = result.rows[0].category_id;
+          return client.query(Query.CategoryQuery, [category]);
+        })
+        .then(results => {
+          client.release();
+          return results;
+        });
     })
     .catch(err => {
+      client.release();
       console.log(err);
     });
 };
 
 const getFeaturedData = (productId) => {
-  return client.query('SELECT category_id FROM products WHERE id = $1;', [productId])
-    .then(result => {
-      var category = result.rows[0].category_id;
-      return client.query(Query.FeaturedQuery, [category]);
+  return pool.connect()
+    .then(client => {
+      return client.query('SELECT category_id FROM products WHERE id = $1;', [productId])
+        .then(result => {
+          var category = result.rows[0].category_id;
+          return client.query(Query.FeaturedQuery, [category]);
+        })
+        .then(results => {
+          client.release();
+          return results;
+        });
     })
     .catch(err => {
+      client.release();
       console.log(err);
     });
 };
 
 const createProduct = ({name, price, image, featured, visited, categoryId, typeId}) => {
   var values = [name, price, image, featured, visited, categoryId, typeId];
-  return client.query(Query.create, values);
+  return pool.query(Query.create, values);
 };
 
 const updateProduct = (productId, field, val) => {
   var values = [val, productId];
-  return client.query(Query.update(field), values);
+  return pool.query(Query.update(field), values);
 };
 
 const deleteProduct = (productId) => {
-  return client.query(Query.remove, [productId]);
+  return pool.query(Query.remove, [productId]);
 };
 
 module.exports = {
